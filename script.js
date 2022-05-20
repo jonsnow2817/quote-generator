42e4f02a554b8413174d0739537d69658d494a42
Mazdi Abo. Soraja <mazdi.soraja@pro.zone>
2022-05-20 14:25:00 +0200

    18. Fetching Data with Javascript

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-<
+// Get Quotes From API
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+
+}
+
+async function getQuotes() {
+
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+
+    } catch (error) {
+        
+        //Catch Error here
+    }
+
+}
+
+// On Load
+getQuotes();
