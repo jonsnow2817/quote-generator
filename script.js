6dd34018709f75ddd4cc01b15e416984fbb5e5cb
Mazdi Abo. Soraja <mazdi.soraja@pro.zone>
2022-05-20 15:45:47 +0200

    Manipulationg the DOM with JavaScript!

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 // Get Quotes From API
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('quote-container');
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
 
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    quoteText.textContent = quote.text;
+    //checking if author fild is blank and replace with 'unknown'
+    if(!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+       
+
+    // Check Quote length to determine stylling
+    if(quote.text.length > 50) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+
 
 }
 
@@ -24,6 +45,17 @@ async function getQuotes() {
     }
 
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+
+newQuoteBtn.addEventListener('click', newQuote );
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
