375d5fb9562200d689287672d67d924cfeb9eab4
Mazdi Abo. Soraja <mazdi.soraja@pro.zone>
2022-05-20 16:06:01 +0200

    Creating Loader with JS

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('quote-container');
+const loader = document.getElementById('loader');
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 
 let apiQuotes = [];
 
@@ -11,39 +25,37 @@ let apiQuotes = [];
 function newQuote() {
 
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    quoteText.textContent = quote.text;
+   
     //checking if author fild is blank and replace with 'unknown'
     if(!quote.author) {
         authorText.textContent = 'Unknown';
     } else {
         authorText.textContent = quote.author;
     }
-       
-
     // Check Quote length to determine stylling
     if(quote.text.length > 50) {
         quoteText.classList.add('long-quote');
     } else {
         quoteText.classList.remove('long-quote')
+
     }
+
+    quoteText.textContent = quote.text;
+    complete();
 
 
 }
 
 async function getQuotes() {
-
     const apiUrl = 'https://type.fit/api/quotes';
     try {
-
+        loading();
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
         newQuote();
-
-    } catch (error) {
-        
+    } catch (error) {        
         //Catch Error here
     }
-
 }
 
 // Tweet Quote
